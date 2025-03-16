b6ddde7a6cd7d9b70e0105d953e6df5beb35e7eb
enegorisan <f04k04369@gmail.com>
2025-03-16 23:46:13 +0900

    input-form-count_practiceの追加

---
+++ input-form-count_practice/js/script.js
@@ -0,0 +1,14 @@
+const text = document.querySelector('#text');
+const count = document.querySelector('#count');
+
+text.addEventListener('keyup', () => {
+ // キー入力された時の処理
+ console.log(text.value.length);
+ count.textContent = text.value.length;
+
+ if (text.value.length > 100) {
+  count.classList.add('alert');
+ } else {
+  count.classList.remove('alert');
+ }
+});
