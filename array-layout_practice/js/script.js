359748cb64404a98105a9da4e0407fe432c8895a
f04k04369 <curb369@gmail.com>
2025-03-29 00:59:20 +0900

    再コミット

--- array-layout_practice/js/script.js
+++ array-layout_practice/js/script.js
@@ -1,19 +1,43 @@
 const menu = document.querySelector('#menu');
 
+
 const lists = [
- 'budou.jpeg',
- 'lemon.jpeg',
- 'mango.jpeg',
- 'raim.jpeg',
- 'apple.jpeg',
- 'strawberry.jpeg'
+ {name: 'イチゴ',
+  img: 'strawberry.jpeg',
+  price: 450},
+ {
+  name: 'レモン',
+  img: 'lemon.jpeg',
+  price: 400
+ },
+ {
+  name: 'ぶどう',
+  img: 'budou.jpeg',
+  price: 350
+ },
+ {
+  name: 'マンゴー',
+  img: 'mango.jpeg',
+  price: 300
+ },
+ {
+  name: 'りんご',
+  img: 'apple.jpeg',
+  price: 250
+ },
+ {
+  name: 'ライム',
+  img: 'raim.jpeg',
+  price: 200
+ }
 ];
 
 console.log(lists);
 
 for (let i = 0; i < lists.length; i++) {
+  const {name,img,price} = lists[i];
   const content = `
-   <div><img src="images/${lists[i]}" alt=""></div>
+   <div><img src="images/${img}" alt=""><h2>${name}</h2><p>${price}</p></div>
   `;
   // menu.textContet = content;
   menu.insertAdjacentHTML('beforeend', content);
---
+++ fadeinanimation/js/script.js
@@ -0,0 +1,7 @@
+const heading  = document.querySelector('#heading');
+
+const Keyframes = {
+ opacity: [0, 1],
+};
+console.log(Keyframes);
+heading.animate(Keyframes, 2000);
