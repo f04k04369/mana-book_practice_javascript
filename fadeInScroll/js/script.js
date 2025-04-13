cc0514ba4e14a341dfe0b18f3f72b2a25480a393
f04k04369 <curb369@gmail.com>
2025-04-13 23:11:38 +0900

    fadeInScrollの追加

---
+++ fadeInScroll/js/script.js
@@ -0,0 +1,10 @@
+// 監視対象が範囲内に現れたら実行する動作
+const showKirin = (entries) => {
+ console.log(entries);
+};
+
+// 監視ロボットの設定
+const kirinObserver = new IntersectionObserver(showKirin);
+
+// #kirinを監視するように指示
+kirinObserver.observe(document.querySelector('#kirin'));
