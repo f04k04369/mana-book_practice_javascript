3e16bc276caf14ed6ae7aecb0e140c4950479f48
f04k04369 <curb369@gmail.com>
2025-04-21 18:43:21 +0900

    グリーンのスクリーンが上がってくるアニメーション実装

--- fasionSite/js/script.js
+++ fasionSite/js/script.js
@@ -2,17 +2,31 @@
 ローディングからの画面遷移
 ================================================ */
 const loadingAreaGrey = document.querySelector('#loading');
+const loadingAreaGreen = document.querySelector('#loading-screen');
 
 window.addEventListener('load', () => {
  // ローディング中（グレースクリーン）
  loadingAreaGrey.animate(
   {
-   opacity: (1, 0),
+   opacity: [1, 0],
    visibility: 'hidden',
   },
   {
    duration: 2000,
    delay: 1200,
+   easing: 'ease',
+   fill: 'forwards',
+  }
+ );
+
+ // ローディング中（薄緑スクリーン）
+ loadingAreaGreen.animate(
+  {
+   translate: ['0 100vh', '0 0', '0 -100vh']
+  },
+  {
+   duration: 2000,
+   delay: 800,
    easing: 'ease',
    fill: 'forwards',
   }
